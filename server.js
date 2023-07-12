b36f31800b51c8bf7be7d0bcc1122e1dedd1ba99
Jason McGrath <jnomad21@yahoo.com>
2023-07-12 11:24:00 -0700

    adding middleware

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const morgan = require('morgan')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(methodOverride('_method'))
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter)
 app.use('/', usersRouter)
