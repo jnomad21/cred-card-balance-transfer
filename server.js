048973163db662ea9f329bfe145780f97b3ba541
Jason McGrath <jnomad21@yahoo.com>
2023-07-12 11:20:49 -0700

    setting up routing

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use('/', indexRouter)
+app.use('/', usersRouter)
 
 app.get('/', function (req, res) {
     res.render('home')
